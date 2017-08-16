50cafc69ce256b17ffa449a2ef956589d61ee197
ivaniesta14 <ivaniesta14@users.noreply.github.com>
2017-08-17 00:09:51 +0200

    math.hpp update
    Finished clamp function
    Added constexpr identifier to max and min
    Added factorial

--- math.hpp
+++ math.hpp
@@ -6,20 +6,41 @@
 namespace i14extra{
 
 template<typename T, typename U>
-const T& min(const T& alpha, const U& beta){
-	static_assert(std::is_arithmetic<T>::value, "i14extra: min: T must be arithmetic!");
-	static_assert(std::is_arithmetic<U>::value, "i14extra: min: U must be arithmetic!");
+constexpr const T& min(const T& alpha, const U& beta){
+	static_assert(std::is_arithmetic<T>::value, "i14extra::min: T must be arithmetic!");
+	static_assert(std::is_arithmetic<U>::value, "i14extra::min: U must be arithmetic!");
 	if(alpha < T(beta))
 		return alpha;
 	return beta;
 }
 template<typename T, typename U>
-const T& max(const T& alpha, const U& beta){
-	static_assert(std::is_arithmetic<T>::value, "i14extra: max: T must be arithmetic!");
-	static_assert(std::is_arithmetic<U>::value, "i14extra: max: U must be arithmetic!");
+constexpr const T& max(const T& alpha, const U& beta){
+	static_assert(std::is_arithmetic<T>::value, "i14extra::max: T must be arithmetic!");
+	static_assert(std::is_arithmetic<U>::value, "i14extra::max: U must be arithmetic!");
 	if(alpha > T(beta))
 		return alpha;
 	return beta;
+}
+template<typename T, typename U, typename V>
+constexpr T& clamp(T& target, const U& low, const V& high){
+	static_assert(std::is_arithmetic<T>::value, "i14extra::clamp: T must be arithmetic!");
+	static_assert(std::is_arithmetic<U>::value, "i14extra::clamp: U must be arithmetic!");
+	static_assert(std::is_arithmetic<V>::value, "i14extra::clamp: V must be arithmetic!");
+	using i14extra::min;
+	using i14extra::max;
+	if(min(target,high)==high){
+		target=high;
+	}else if(max(target,low)==low){
+		target=low;
+	}
+	return target;
+}
+template<typename T>
+constexpr unsigned factorial(T value){
+	static_assert(std::is_unsigned<T>::value, "i14extra::factorial: T must be unsigned!");
+	if(value==0)
+		return 1;
+	return value * factorial(value-1);
 }
 
 }
