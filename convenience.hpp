3471936a574820d8020afa5e6cb047f6cd519ba7
Ivan <ivaniesta14@gmail.com>
2017-12-12 18:52:12 +0100

    Update convenience.hpp
    Sorted convenience typedef's by size
    Added I_NO_DEF_CONS macro to define default constructor as deleted.
    Added DEFAULTED macro to mark ctor|dtor as defaulted.
    Now enum_unoredered_map and _set are only created if unordered_map and _set are included beforehand.
    Added convenience using's for unordered maps and sets.
    Added convenience functions to check for (in)equality.
    Added define's for all comparison operator member functions
    Added define's for references, const references and const volatile.
    Added getter and setter function factories.
    Added macro for std::get

--- convenience.hpp
+++ convenience.hpp
@@ -1,16 +1,13 @@
 #ifndef I14EXTRA_CONVENIENCE_HPP
 #define I14EXTRA_CONVENIENCE_HPP
 
-#include <unordered_map>
-#include <unordered_set>
-
 namespace i14extra{
 
 typedef signed char schar;
 typedef signed short sshort;
 typedef signed sint;
-typedef signed long long slonglong, sllong, llong;
 typedef signed long slong;
+typedef signed long long slonglong, sllong, llong;
 typedef unsigned long long ulonglong, ullong;
 typedef long double ldouble;
 #ifndef QGLOBAL_H
@@ -31,16 +28,37 @@ using ulong=::ulong;
     c(const c& other)=delete;\
     const c& operator=(const c& rhs)=delete
 #define nocopy(c) I_NO_COPY(c)
-template<typename E,typename T>
-using enum_unordered_map=std::unordered_map<E,T,std::hash<int>>;
-template<typename E>
-using enum_unordered_set=std::unordered_set<E,std::hash<int>>;
-template<typename E,typename T>
-using enum_unordered_multimap=std::unordered_multimap<E,T,std::hash<int>>;
-template<typename E>
-using enum_unordered_multiset=std::unordered_multiset<E,std::hash<int>>;
+
+#define I_NO_DEF_CONS(c) \
+    c()=delete;
+#define nodef(c) I_NO_DEF_CONS(c)
+
 #define DELETED =delete
 #define deleted DELETED
+#define DEFAULTED =default
+#define defaulted DEFAULTED
+
+
+#ifdef _GLIBCXX_UNORDERED_MAP
+template<typename E,typename T>
+using enum_unordered_map=std::unordered_map<E,T,std::hash<int>>;
+template<typename E,typename T>
+using enum_unordered_multimap=std::unordered_multimap<E,T,std::hash<int>>;
+template<typename T,typename U> using umap=std::unordered_map<T,U>;
+template<typename E,typename T> using enum_umap=enum_unordered_map<E,T>;
+template<typename T,typename U> using ummap=std::unordered_multimap<T,U>;
+template<typename E,typename T> using enum_ummap=enum_unordered_multimap<E,T>;
+#endif //_GLIBCXX_UNORDERED_MAP
+#ifdef _GLIBCXX_UNORDERED_SET
+template<typename E>
+using enum_unordered_set=std::unordered_set<E,std::hash<int>>;
+template<typename E>
+using enum_unordered_multiset=std::unordered_multiset<E,std::hash<int>>;
+template<typename T> using uset=std::unordered_set<T>;
+template<typename T> using enum_uset=enum_unordered_set<T>;
+template<typename T> using umset=std::unordered_multiset<T>;
+template<typename T> using enum_umset=enum_unordered_multiset<T>;
+#endif //_GLIBCXX_UNORDERED_SET
 
 template<typename T,typename U> constexpr bool gt(const T& lhs, const U& rhs){
     return lhs>rhs;
@@ -56,6 +74,57 @@ template<typename T,typename U>
 constexpr bool not_lt(const T& lhs,const U& rhs){
     return not(gt(rhs,lhs));
 }
+template<typename T,typename U> constexpr bool eq(const T& lhs,const U& rhs){
+    return lhs==rhs;
+}
+template<typename T,typename U> constexpr bool neq(const T& lhs,const U& rhs){
+    return lhs!=rhs;
+}
+
+#define I_AUTO_NOT_EQ(left,right) \
+    bool operator!=(const left& lhs,const right& rhs){\
+        return !(lhs==rhs);\
+    }
+#define I_AUTO_LT(left, right)\
+    bool operator<(const left& lhs,const right& rhs){\
+        return lhs<rhs;\
+    }
+#define I_AUTO_GT(left, right) I_AUTO_LT(right,left)
+#define I_AUTO_LT_EQ(left, right)\
+    bool operator<=(const left& lhs,const right& rhs){\
+        return !(lhs>rhs);\
+    }
+#define I_AUTO_RT_EQ(left,right) I_AUTO_LT_EQ(right,left)
+
+#define i_not_eq(left,right) I_AUTO_NOT_EQ(left,right)
+#define i_lt(left, right) I_AUTO_LT(left,right)
+#define i_gt(left, right) I_AUTO_GT(left, right)
+#define i_lt_eq(left, right) I_AUTO_LT_EQ(left,right)
+#define i_gt_eq(left, right) I_AUTO_GT_EQ(left,right)
+
+#define ref &
+#define lref &
+#define cref const &
+#define cv const volatile
+
+#define I_AUTO_GETTER(type,from,name,Name)\
+    type get##Name(){\
+        return from.name;\
+    }
+#define I_AUTO_SETTER(type,to,name,Name)\
+    type ref set##Name(type cref val){\
+        to.name=val;\
+        return *this;\
+    }
+#define I_AUTO_VOID_SETTER(type,to,name,Name)\
+    void set##Name(type cref val){\
+        to.name=val;\
+    }
+#define getter(type,from,name,Name) I_AUTO_GETTER(type,from,name,Name)
+#define setter(type,to,name,Name) I_AUTO_SETTER(type,to,name,Name)
+#define void_setter(type,to,name,Name) I_AUTO_VOID_SETTER(type,to,name,Name)
+
+#define sget std::get
 
 }//namespace i14extra
 
